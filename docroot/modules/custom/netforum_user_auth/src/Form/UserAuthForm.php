08f96f3d6a6a7c3897376b4c40708cf4ce920d11
Matthew Luzitano <matthew@bluecoda.com>
2017-07-24 09:49:11 -0400

    MSCA-50 | Better inline documentation

--- docroot/modules/custom/netforum_user_auth/src/Form/UserAuthForm.php
+++ docroot/modules/custom/netforum_user_auth/src/Form/UserAuthForm.php
@@ -17,7 +17,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class UserAuthForm extends FormBase {
 
-
   //Typical Drupal $user object
   protected $userStorage;
 
@@ -69,13 +68,6 @@ class UserAuthForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Display result.
     foreach ($form_state->getValues() as $key => $value) {
@@ -90,6 +82,8 @@ class UserAuthForm extends FormBase {
     if(!empty($email) && !empty($password)) {
       $auth = $this->Auth($email, $password);
       if($auth) {
+        //At some point in the project, we may need to make this configurable,
+        //but for now this seems the most common scenario.
         $form_state->setRedirect('<front>');
         return;
       } else {
@@ -98,6 +92,11 @@ class UserAuthForm extends FormBase {
     }
   }
 
+  //Creates a new user account with the credentials passed in the NetForum Login
+  //form. We may need to pull in more information like First Name and Last Name,
+  //in which case this module should be updated to include a $params array as
+  //the last argument so we can easily loop through it and create the user account
+  //with those fields.
   private function createUserFromNetForumUser($email, $password) {
     $created_user = $this->userStorage->create([
       'email' => $email,
@@ -139,9 +138,11 @@ class UserAuthForm extends FormBase {
     $auth_headers = $this->get_client->getAuthHeaders();
     $response_headers = $this->get_client->getResponseHeaders();
     try {
+      //CheckEWebUser simply attempts to authenticate based on the passed credentials.
       $response = $client->__soapCall('CheckEWebUser', array('parameters' => $params), NULL, $auth_headers, $response_headers);
       if (!empty($response->CheckEWebUserResult->any)) {
         $xml = simplexml_load_string($response->CheckEWebUserResult->any);
+        //Could probably be better handled with the Serialization API
         $json = json_encode($xml);
         $array = json_decode($json, TRUE);
         if (!empty($array['@attributes']['recordReturn']) && $array['@attributes']['recordReturn'] == '1') {
