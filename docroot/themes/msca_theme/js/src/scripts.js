2b7104f200c8076985eb9484717897170a546e84
annebonham <abonham@mollyduggan.com>
2017-11-17 18:38:01 -0800

    MSCA-206 - global search modal

--- docroot/themes/msca_theme/js/src/scripts.js
+++ docroot/themes/msca_theme/js/src/scripts.js
@@ -3,25 +3,9 @@
 (function ($, Drupal) {
   Drupal.behaviors.msca_theme = {
     attach: function (context, settings) {
-
-      // Todo: implement panels for this.
-      var node_classes = [
-        'page-node-type-facility',
-        'page-node-type-newsletter-article',
-        'page-node-type-newsroom',
-        'page-node-type-page',
-        'page-node-type-resources',
-        'page-node-type-scholarships',
-        'page-node-type-video'
-      ];
-
-      var body = $('body');
-
-      $(node_classes).once().each(function() {
-
-        if (body.hasClass(this)) {
-          $('div.newsletter-article-bottom-row').appendTo('.main-container > .row');
-        }
+      // Moving search block into masthead search menu link so bootstrap dropdown is happy.
+      $('div#block-searchform', context).once(function() {
+          $('div#block-searchform').appendTo('.search.nav-item');
       });
 
     }
