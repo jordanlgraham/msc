06ee4a48a57f9683f1b87caa8afabdbe80f5736e
annebonham <abonham@mollyduggan.com>
2017-11-22 16:57:07 -0800

    MSCA-266 - various lisitng views and dependent config, template and styles. Also removing unnecessary Topic fields

--- docroot/themes/msca_theme/scripts.js
+++ docroot/themes/msca_theme/scripts.js
@@ -609,8 +609,8 @@ var Tab = function ($) {
       $(context).find('.has-submenu').once('has-submenu').each(function () {
         $(this).click(function (e) {
           if ($(e.target).closest('li').hasClass('has-submenu')) {
-            e.preventDefault();
-            e.stopPropagation(); // Keeps parent class from toggling if subnav is clicked
+            // e.preventDefault();
+            // e.stopPropagation(); // Keeps parent class from toggling if subnav is clicked
             // Find the parent of the current link and toggle it's immediate child UL.
             // Another safeguard to keep parent classes from toggling
             $(e.target).closest('li').toggleClass('submenu-open');
