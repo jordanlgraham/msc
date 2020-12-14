294d9840a89002dbf92beabaea1d1a7617135dad
Jordan Graham <jordan@handsomedogstudio.com>
2020-12-13 22:10:08 -0700

    MSC-2435: Updated unsupported version of easy_breadcrumb to 8.x-1.13.

--- docroot/modules/contrib/easy_breadcrumb/src/EasyBreadcrumbConstants.php
+++ docroot/modules/contrib/easy_breadcrumb/src/EasyBreadcrumbConstants.php
@@ -13,6 +13,15 @@ class EasyBreadcrumbConstants {
   const MODULE_NAME = 'easy_breadcrumb';
 
   /**
+   * Module's settings.
+   */
+  const MODULE_SETTINGS = 'easy_breadcrumb.settings';
+  /**
+   * Flag for applying easy breadcrumb to admin routes.
+   */
+  const APPLIES_ADMIN_ROUTES = 'applies_admin_routes';
+
+  /**
    * Flag for including invalid paths while generating the breadcrumb segments.
    */
   const INCLUDE_INVALID_PATHS = 'include_invalid_paths';
@@ -21,6 +30,16 @@ class EasyBreadcrumbConstants {
    * List of paths to be excluded from the generated segments.
    */
   const EXCLUDED_PATHS = 'excluded_paths';
+
+  /**
+   * List of titles to replace.
+   */
+  const REPLACED_TITLES = 'replaced_titles';
+
+  /**
+   * List of paths for custom breadcrumbs.
+   */
+  const CUSTOM_PATHS = 'custom_paths';
 
   /**
    * Separator between segments.
@@ -36,6 +55,11 @@ class EasyBreadcrumbConstants {
    * Title for the front page segment.
    */
   const HOME_SEGMENT_TITLE = 'home_segment_title';
+
+  /**
+   * Flag for keeping the breadcrumb on the front page.
+   */
+  const HOME_SEGMENT_KEEP = 'home_segment_keep';
 
   /**
    * Flag for including or not the page's title as a segment.
@@ -63,6 +87,21 @@ class EasyBreadcrumbConstants {
   const CAPITALIZATOR_IGNORED_WORDS = 'capitalizator_ignored_words';
 
   /**
+   * List of words to be forced by the 'capitalizator'. E.g.: your brand's name.
+   */
+  const CAPITALIZATOR_FORCED_WORDS = 'capitalizator_forced_words';
+
+  /**
+   * List of words to be forced by the 'capitalizator'. E.g.: your brand's name.
+   */
+  const CAPITALIZATOR_FORCED_WORDS_FIRST_LETTER = 'capitalizator_forced_words_first_letter';
+
+  /**
+   * Logical value to 'Make the first letters of each segment capitalized'.
+   */
+  const CAPITALIZATOR_FORCED_WORDS_CASE_SENSITIVITY = 'capitalizator_forced_words_case_sensitivity';
+
+  /**
    * Flag for showing the language prefix as its own segment.
    */
   const LANGUAGE_PATH_PREFIX_AS_SEGMENT = 'language_path_prefix_as_segment';
@@ -73,9 +112,39 @@ class EasyBreadcrumbConstants {
   const USE_MENU_TITLE_AS_FALLBACK = 'use_menu_title_as_fallback';
 
   /**
+   * Use page title as fallback for menu title.
+   */
+  const USE_PAGE_TITLE_AS_MENU_TITLE_FALLBACK = 'use_page_title_as_menu_title_fallback';
+
+  /**
+   * Use site title as the front page segment.
+   */
+  const USE_SITE_TITLE = 'use_site_title';
+
+  /**
    * Flag for removing repeated identical segments from the breadcrumb.
    */
   const REMOVE_REPEATED_SEGMENTS = 'remove_repeated_segments';
+
+  /**
+   * Flag for storing absolute path settings.
+   */
+  const ABSOLUTE_PATHS = 'absolute_paths';
+
+  /**
+   * Flag for storing single home item settings.
+   */
+  const HIDE_SINGLE_HOME_ITEM = 'hide_single_home_item';
+
+  /**
+   * Flag for using term hierarchy.
+   */
+  const TERM_HIERARCHY = 'term_hierarchy';
+
+  /**
+   * Flag for adding the breadcrumb as structured to the HTML head.
+   */
+  const ADD_STRUCTURED_DATA_JSON_LD = 'add_structured_data_json_ld';
 
   /**
    * Default list of excluded paths.
@@ -84,32 +153,34 @@ class EasyBreadcrumbConstants {
    *   Default list of ignored paths.
    */
   public static function defaultExcludedPaths() {
-    static $default_excluded_paths = array(
+    static $default_excluded_paths = [
       'search',
       'search/node',
-    );
+    ];
 
     return $default_excluded_paths;
   }
 
   /**
-   * Default list of ignored words.
+   * Default list of replaced titles.
    *
    * @return array
-   *   Default list of ignored words.
+   *   Default list of replaced titles.
    */
-  public static function defaultIgnoredWords() {
-    static $default_ignored_words = array(
-      'of',
-      'and',
-      'or',
-      'de',
-      'del',
-      'y',
-      'o',
-    );
+  public static function defaultReplacedTitles() {
 
-    return $default_ignored_words;
+    return [];
+  }
+
+  /**
+   * Default list of replaced paths.
+   *
+   * @return array
+   *   Default list of replaced paths.
+   */
+  public static function defaultCustomPaths() {
+
+    return [];
   }
 
 }
