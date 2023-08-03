69b1b536177e0b2402b6ca7dbb3adcd6ea39e592
Jordan Graham <jordan@atendesigngroup.com>
2023-08-03 08:53:33 -0600

    MSC-2900: Updated post-code-deploy script, defining app root.

--- hooks/common/post-code-deploy/deploy.sh
+++ hooks/common/post-code-deploy/deploy.sh
@@ -7,16 +7,18 @@ deployed_tag="$4"
 repo_url="$5"
 repo_type="$6"
 drush_alias=${site}.${target_env}
+# Set the app_root to the document root of the website.
+app_root="/var/www/html/${site}"
 
 SECONDS=0
 echo "Clearing caches..."
-/usr/local/bin/drush @${drush_alias} cr --strict=0
+/usr/local/bin/drush --root="${app_root}" @${drush_alias} cr --strict=0
 echo "Caches cleared in $SECONDS seconds."
 SECONDS=0
 echo "Applying DB updates..."
-/usr/local/bin/drush @${drush_alias} updb -y --strict=0
+/usr/local/bin/drush --root="${app_root}" @${drush_alias} updb -y --strict=0
 echo "DB updates run in $SECONDS seconds."
 SECONDS=0
 echo "Importing configuration..."
-/usr/local/bin/drush @${drush_alias} cim -y --strict=0
+/usr/local/bin/drush --root="${app_root}" @${drush_alias} cim -y --strict=0
 echo "Configuration imported in $SECONDS seconds."
