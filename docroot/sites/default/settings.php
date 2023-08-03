c85712c59a86638bd0f832a68c81e8e3789ac1af
Jordan Graham <jordan@atendesigngroup.com>
2023-08-03 10:46:50 -0600

    MSC-2901: Updated settings.php with a different method of disabling shield in the production environment.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -801,7 +801,7 @@ if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
       case 'prod':
       // Disable Shield on prod by setting the
       // shield user variable to NULL
-      $config['shield.settings']['credentials']['shield']['user'] = NULL;
+      $config['shield.settings']['shield_enable'] = FALSE;
       break;
   }
   // Set the hash salt.
