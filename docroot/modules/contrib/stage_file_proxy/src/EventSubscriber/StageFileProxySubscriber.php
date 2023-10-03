0a16c22abbd754814973b583444e9ca7086dfc8e
Jordan Graham <jordan@atendesigngroup.com>
2023-10-03 07:28:42 -0600

    MSC-2914: Updated stage_file_proxy to 2.1.1 for Drupal 10 compatibility.

--- docroot/modules/contrib/stage_file_proxy/src/EventSubscriber/StageFileProxySubscriber.php
+++ docroot/modules/contrib/stage_file_proxy/src/EventSubscriber/StageFileProxySubscriber.php
@@ -6,8 +6,8 @@ use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\StreamWrapper\StreamWrapperManager;
 use Drupal\Core\Url;
+use Drupal\stage_file_proxy\DownloadManagerInterface;
 use Drupal\stage_file_proxy\EventDispatcher\AlterExcludedPathsEvent;
-use Drupal\stage_file_proxy\FetchManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -17,18 +17,13 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Stage file proxy subscriber for controller requests.
- *
- * @deprecated in stage_file_proxy:2.1.0 and is removed from stage_file_proxy:3.0.0.
- *   Use StageFileProxySubscriber instead.
- *
- * @see https://www.drupal.org/project/stage_file_proxy/issues/3282542
  */
-class ProxySubscriber implements EventSubscriberInterface {
+class StageFileProxySubscriber implements EventSubscriberInterface {
 
   /**
    * The manager used to fetch the file against.
    *
-   * @var \Drupal\stage_file_proxy\FetchManagerInterface
+   * @var \Drupal\stage_file_proxy\DownloadManagerInterface
    */
   protected $manager;
 
@@ -63,7 +58,7 @@ class ProxySubscriber implements EventSubscriberInterface {
   /**
    * Construct the FetchManager.
    *
-   * @param \Drupal\stage_file_proxy\FetchManagerInterface $manager
+   * @param \Drupal\stage_file_proxy\DownloadManagerInterface $manager
    *   The manager used to fetch the file against.
    * @param \Psr\Log\LoggerInterface $logger
    *   The logger interface.
@@ -74,7 +69,7 @@ class ProxySubscriber implements EventSubscriberInterface {
    * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
    *   The request stack.
    */
-  public function __construct(FetchManagerInterface $manager, LoggerInterface $logger, EventDispatcherInterface $event_dispatcher, ConfigFactoryInterface $config_factory, RequestStack $request_stack) {
+  public function __construct(DownloadManagerInterface $manager, LoggerInterface $logger, EventDispatcherInterface $event_dispatcher, ConfigFactoryInterface $config_factory, RequestStack $request_stack) {
     $this->manager = $manager;
     $this->logger = $logger;
     $this->eventDispatcher = $event_dispatcher;
@@ -85,10 +80,12 @@ class ProxySubscriber implements EventSubscriberInterface {
   /**
    * Fetch the file from it's origin.
    *
-   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   * @param \Symfony\Component\HttpKernel\Event\ResponseEvent $event
    *   The event to process.
+   *
+   * @todo Drop GetRequestEvent typehint when dropping Drupal 9 support.
    */
-  public function checkFileOrigin(RequestEvent $event) {
+  public function checkFileOrigin(RequestEvent|GetRequestEvent $event) {
     $config = $this->configFactory->get('stage_file_proxy.settings');
 
     // Get the origin server.
@@ -123,17 +120,6 @@ class ProxySubscriber implements EventSubscriberInterface {
       return;
     }
 
-    // Quit if the extension is in the list of excluded extensions.
-    $excluded_extensions = $config->get('excluded_extensions') ?
-      array_map('trim', explode(',', $config->get('excluded_extensions'))) : [];
-
-    $path_info = pathinfo($request_path);
-    $ext = $path_info['extension'];
-
-    if (in_array($ext, $excluded_extensions)) {
-      return;
-    }
-
     $alter_excluded_paths_event = new AlterExcludedPathsEvent([]);
     $this->eventDispatcher->dispatch($alter_excluded_paths_event, 'stage_file_proxy.alter_excluded_paths');
     $excluded_paths = $alter_excluded_paths_event->getExcludedPaths();
@@ -146,31 +132,38 @@ class ProxySubscriber implements EventSubscriberInterface {
     // Note if the origin server files location is different. This
     // must be the exact path for the remote site's public file
     // system path, and defaults to the local public file system path.
-    $origin_dir = $config->get('origin_dir') ?? '';
-    $remote_file_dir = trim($origin_dir);
-    if (!empty($remote_file_dir)) {
+    $remote_file_dir = trim($config->get('origin_dir'));
+    if (!$remote_file_dir) {
       $remote_file_dir = $file_dir;
     }
 
     $request_path = rawurldecode($request_path);
     // Path relative to file directory. Used for hotlinking.
-    $relative_path = mb_substr($request_path, mb_strlen($file_dir));
+    $relative_path = mb_substr($request_path, mb_strlen($file_dir) + 1);
     // If file is fetched and use_imagecache_root is set, original is used.
     $paths = [$relative_path];
 
     // Webp support.
-    if (str_ends_with($relative_path, '.webp')) {
+    $is_webp = FALSE;
+    if (strpos($relative_path, '.webp')) {
       $paths[] = str_replace('.webp', '', $relative_path);
-      $paths = array_reverse($paths);
+      $is_webp = TRUE;
     }
 
     foreach ($paths as $relative_path) {
       $fetch_path = $relative_path;
 
+      // Don't touch CSS and JS aggregation. 'css/' and 'js/' are hard coded to
+      // match route definitions.
+      // @see \Drupal\system\Routing\AssetRoutes
+      if (str_starts_with($relative_path, 'css/') || str_starts_with($relative_path, 'js/')) {
+        return;
+      }
+
       // Is this imagecache? Request the root file and let imagecache resize.
       // We check this first so locally added files have precedence.
       $original_path = $this->manager->styleOriginalPath($relative_path, TRUE);
-      if ($original_path) {
+      if ($original_path && !$is_webp) {
         if (file_exists($original_path)) {
           // Imagecache can generate it without our help.
           return;
@@ -185,6 +178,7 @@ class ProxySubscriber implements EventSubscriberInterface {
       $query_parameters = UrlHelper::filterQueryParameters($query);
       $options = [
         'verify' => $config->get('verify'),
+        'query' => $query_parameters,
       ];
 
       if ($config->get('hotlink')) {
